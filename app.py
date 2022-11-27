cc0433209e2f5949aa1a374c64a73d0baf2ad1d6
Yudusty <henriqueyudi7@gmail.com>
2022-11-27 21:30:00 +0000

    Adição do campo avaliação

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask , render_template , request
 app = Flask(__name__)
 
 tasks = [
-    {'name' : 'estudar' , 'finished' : False},
-    {'name' : 'dormir' , 'finished' : True}
+    {'name' : 'estudar' , 'avaliacao' : '2/10'},
+    {'name' : 'dormir' , 'avaliacao' : '10/10'}
 ]
 
 @app.route('/')
@@ -15,9 +15,16 @@ def home():
 @app.route('/create' , methods=['POST'])
 def create():
     name = request.form['name']
+    avaliacao = request.form['avaliacao']
     #number = request.form['number']
-    task = {'name' : name, 'finished': False}
+    task = {'name' : name, 'avaliacao': avaliacao}
     tasks.append(task)
+    return render_template('home.html' , tasks=tasks)
+
+@app.route('/delete', methods=['DELETE'])
+def delete(task):
+    task = {'name' : name, 'avaliacao': avaliacao}
+    tasks.delete(tasks)
     return render_template('home.html' , tasks=tasks)
 
 app.run(debug=True)

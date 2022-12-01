cf002c1454a506cd485ff7a1f62e94a0e826d790
Yudusty <henriqueyudi7@gmail.com>
2022-12-01 16:58:02 +0000

    Adicionando a ação de Edit do site

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ filmes = []
 @app.route('/')
 def index():
     with open('filmes.csv', 'rt') as file_in:
-        dados = csv.DictReader(fole_in)
+        dados = csv.DictReader(file_in)
         return render_template('home.html', dados=dados)
 
 @app.route('/html')
@@ -33,6 +33,41 @@ def create():
         usuario = csv.writer(file_out)
         usuario.writerows(filmes)
     
+    #Redireciona as variáveis para '/'
+    with open('filmes.csv', 'rt') as file_in:
+        dados = csv.DictReader(file_in)
+        return render_template('home.html', dados=dados)
+
+#Pega as variáveis para modificar
+@app.route('/edit/<nome>/<avaliacao>')
+def edit(nome,avaliacao):
+    editList = [nome,avaliacao]
+    return render_template('editDelete.html', editList=editList)
+
+#Onde salva as variáveis modificadas
+@app.route('/saveEdit', methods=['SAVE'])
+def saveEdit():
+
+    #Inseri as novas variáveis
+    nome = request.form['nome']
+    avaliacao = request.form['avaliacao']
+
+    #Abre o DataFrame do .csv
+    dataFrame = pd.read.csv('filmes.csv')
+
+    #Criação do novo DataFrame
+    new_dataFrame = pd.DataFRame({'nome': [nome],'avaliacao': [avaliacao]})
+
+    #Colocar os index para a coluna 'nome'
+    dataFrame = dataFrame.set_index('nome')
+    new_dataFrame = new_dataFrame.set_index("nome")
+
+    #Atualiza o DataFrame
+    dataFrame.update(new_dataFrame)
+
+    #Salva o nome DataFrame no .csv
+    dataFrame.to_csv('filmes.csv')
+
     #Redireciona as variáveis para '/'
     with open('filmes.csv', 'rt') as file_in:
         dados = csv.DictReader(file_in)

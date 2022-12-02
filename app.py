9d5924ddfecfc500473ebd3cd396a45bb24260d2
Yudusty <henriqueyudi7@gmail.com>
2022-12-02 03:40:11 +0000

    Modificação e ajustes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request
 from uuid import uuid4
 import pandas as pd
 import csv
@@ -6,92 +6,102 @@ import csv
 
 app = Flask(__name__)
 
-filmes = []
 
+# le o itens.csv e joga para o index.html
 @app.route('/')
 def index():
     with open('filmes.csv', 'rt') as file_in:
-        dados = csv.DictReader(file_in)
-        return render_template('home.html', dados=dados)
+        filmes = csv.DictReader(file_in)
+        return render_template('home.html', filmes=filmes)
 
-@app.route('/html')
-def home(): 
-    #templates
-    return render_template('create.html' , filmes=filmes)
 
-#Salva as variáveis no form em um arquivo.csv
-@app.route('/create' , methods=['POST'])
+#sei la como funciona (não modificar)
+@app.route('/create')
 def create():
+    return render_template('create.html')
 
-    #Onde puxa as variáveis forms
-    name = request.form['name']
-    avaliacao = request.form['avaliacao']
-    filmes.append([uuid4(), name, avaliacao])
 
-    #Adicionar uma nova row no .csv
-    with open('filmes.csv','a') as file_out:
-        usuario = csv.writer(file_out)
-        usuario.writerows(filmes)
-    
-    #Redireciona as variáveis para '/'
+#salva as variaveis do forms em uma nova row no arquivo .csv
+@app.route('/save', methods=['POST'])
+def save():
+
+    #puxa as variaveis do forms 
+    name = request.form['name']         
+    avaliacao = request.form['avaliacao'] 
+   
+
+    entrada= []
+    entrada.append([uuid4(), name, avaliacao]) #trocar para pandas dataset dps aaaaaaaaaaaaaaa
+
+    #adiciona *append* uma nova row no .csv
+    with open('filmes.csv', 'a') as file_out:
+        escritor = csv.writer(file_out)
+        escritor.writerows(entrada)
+   
+    #redireciona para "/" 
     with open('filmes.csv', 'rt') as file_in:
-        dados = csv.DictReader(file_in)
-        return render_template('home.html', dados=dados)
+        filmes = csv.DictReader(file_in)
+        return render_template('home.html', filmes=filmes)
 
-#Pega as variáveis para modificar
-@app.route('/edit/<nome>/<avaliacao>')
-def edit(nome,avaliacao):
-    editList = [nome,avaliacao]
-    return render_template('edit.html', editList=editList)
 
-#Onde salva as variáveis modificadas
-@app.route('/saveEdit', methods=['SAVE'])
-def saveEdit():
-
-    #Inseri as novas variáveis
-    nome = request.form['nome']
-    avaliacao = request.form['avaliacao']
-
-    #Abre o DataFrame do .csv
-    dataFrame = pd.read.csv('filmes.csv')
-
-    #Criação do novo DataFrame
-    new_dataFrame = pd.DataFRame({'nome': [nome],'avaliacao': [avaliacao]})
-
-    #Colocar os index para a coluna 'nome'
-    dataFrame = dataFrame.set_index('nome')
-    new_dataFrame = new_dataFrame.set_index("nome")
-
-    #Atualiza o DataFrame
-    dataFrame.update(new_dataFrame)
-
-    #Salva o nome DataFrame no .csv
-    dataFrame.to_csv('filmes.csv')
-
-    #Redireciona as variáveis para '/'
-    with open('filmes.csv', 'rt') as file_in:
-        dados = csv.DictReader(file_in)
-        return render_template('home.html', dados=dados)
-
-#Deleta as linhas pelo Id
+#deleta rows de acordo com o Id
 @app.route('/delete/<id>')
 def delete(id):
 
-    #Abre o arquivo pelo pandas
-    dataFrame = pd.read_csv("filmes.csv") 
+    # abre o arquivo .csv pelo pandas
+    data = pd.read_csv("filmes.csv") 
+    #seta o index valeu para a columm 'Id'
+    data = data.set_index("id") 
 
-    #Coloca o index para coluna 'id'
-    dataFrame = dataFrame.set_index("Id") 
-
-    #Pega toda a linha que tiver a mesma variavel "id" na coluna index
-    dataFrame.drop(id, axis='index', inplace=True) 
+    #dropa toda row que tiver a mesma variavel "id" na columm index
+    data.drop(id, axis='index', inplace=True) 
     
-    #Salva o novo DataFrame
-    dataFrame.to_csv('filmes.csv')  
+    #salva o novo dataset
+    data.to_csv('filmes.csv')  
 
-    #Le o arquivo e envia para a variável do Html 
+    # função que le o arquivo e envia a variavel para o html OBS: provavelmente existe alguma outra forma mais simples e eficiente de fazer isso, porem esta funcionando ent deia quieto 
     with open('filmes.csv', 'rt') as file_in:
-        dados = csv.DictReader(file_in)
-        return render_template('home.html', dados=dados)
+        filmes = csv.DictReader(file_in)
+        return render_template('home.html', filmes=filmes)
+    
+
+#pega as variaveis da row que o usuario quer modificar e coloca dentro dos forms
+@app.route('/edit/<id>/<name>/<avaliacao>')
+def update(id,name,avaliacao):#obtem as var pela url 
+    lista = [id,name,avaliacao]#transforma em lista para facilitar
+    return render_template('edit.html', lista=lista) #e joga para o update.html
+
+
+#salva os forms que foram modificados do /update/
+@app.route('/saveedit', methods=['POST'])
+def saveedit():
+
+    #obtem as novas variaveis
+    id = request.form['id'] # o id esta ocultado na pagina
+    name = request.form['name']         
+    avaliacao = request.form['avaliacao'] 
+ 
+
+    #abre o dataframe do .csv
+    data = pd.read_csv("filmes.csv")
+
+    #cria um novo dataframe apartir das novas variaveis
+    new_df = pd.DataFrame({'id': [id],'name': [name],'avaliacao': [avaliacao]})
+
+    #seta os index's para a coluna 'Id'
+    #n sei se isso é necessario mas na minha mente faz sentido 
+    data = data.set_index("id")
+    new_df = new_df.set_index("id")
+
+    #atualiza os dados do data frame antigo com o novo
+    data.update(new_df)
+
+    #salva o arquivo
+    data.to_csv('filmes.csv')
+
+    #redireciona para "/"
+    with open('filmes.csv', 'rt') as file_in:
+        filmes = csv.DictReader(file_in)
+        return render_template('home.html', filmes=filmes)
 
 app.run(debug=True)
